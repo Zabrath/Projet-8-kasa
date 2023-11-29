c6638ba67283f4a3c5cfb7213c6877e24fc53719
Laura <zabrath95@gmail.com>
2023-11-29 17:08:31 +0100

    import routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Home from "./pages/home/home";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <div><Header /><Home /><Footer /></div>
+  },
+  {
+    path:'/blog',
+    element: <div>blog</div>
+  }
+])
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Home />
-      <Footer />
-    </>
-  );
+  return <RouterProvider router={router}/>
 }
 
 export default App;
