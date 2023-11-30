4a81022e518d0165a096eb7cc0fa424c998e865d
Laura <zabrath95@gmail.com>
2023-11-30 13:43:35 +0100

    caroussel composant

---
+++ src/components/caroussel/caroussel.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import "./caroussel.scss";
+
+function Caroussel({ images }) {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const totalImages = images.length;
+
+  const prevImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? totalImages - 1 : prevIndex - 1
+    );
+  };
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === totalImages - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  return (
+    <div className="pictures-carousel">
+      <button className="carousel-arrow arrow__left" onClick={prevImage}>
+        <FontAwesomeIcon icon={faAngleLeft} />
+      </button>
+      <button className="carousel-arrow arrow__right" onClick={nextImage}>
+        <FontAwesomeIcon icon={faAngleRight} />
+      </button>
+      <img src={images[currentImageIndex]} alt="" />
+    </div>
+  );
+}
+
+export default Caroussel;
--- src/pages/fichelogement/fichelogement.jsx
+++ src/pages/fichelogement/fichelogement.jsx
@@ -1,70 +1,21 @@
-// import React from "react";
-// import { useParams } from "react-router-dom";
-// import "./fichelogement.scss";
-
-// function FicheLogement({ logements }) {
-//   const { logementId } = useParams();
-
-//   const logement = logements.find((logement) => logement.id === logementId);
-
-//   if (!logement) {
-//     return <div>Logement non trouvé</div>;
-//   }
-
-//   return (
-//     <div className="fichelogement">
-//       <h1>{logement.title}</h1>
-//       <div className="pictures-carousel">
-//         {logement.pictures.map((picture, index) => (
-//           <img key={index} src={picture} alt={`Picture ${index}`} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default FicheLogement;
-
-import React, { useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import Caroussel from "../../components/caroussel/caroussel";
 import "./fichelogement.scss";
 
 function FicheLogement({ logements }) {
   const { logementId } = useParams();
+
   const logement = logements.find((logement) => logement.id === logementId);
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   if (!logement) {
     return <div>Logement non trouvé</div>;
   }
 
-  const totalImages = logement.pictures.length;
-
-  const prevImage = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? totalImages - 1 : prevIndex - 1
-    );
-  };
-
-  const nextImage = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === totalImages - 1 ? 0 : prevIndex + 1
-    );
-  };
-
   return (
     <main className="fichelogement">
-      <div className="pictures-carousel">
-        <button className="carousel-arrow arrow__left" onClick={prevImage}>
-          <FontAwesomeIcon icon={faAngleLeft} />
-        </button>
-        <button className="carousel-arrow arrow__right" onClick={nextImage}>
-          <FontAwesomeIcon icon={faAngleRight} />
-        </button>
-        <img src={logement.pictures[currentImageIndex]} alt="" />
-      </div>
+      <Caroussel images={logement.pictures} />
+      <h1>{logement.title}</h1>
     </main>
   );
 }
